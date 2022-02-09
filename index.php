3cfe010260f008285b3bcf79b082fc2d32522695
jackdaniel1996 <46755205+jackdaniel1996@users.noreply.github.com>
2022-02-09 22:52:37 +0100

    added api

--- index.php
+++ index.php
@@ -78,8 +78,41 @@
                 <div class="title-font">Crypto Invest Calculator</div>
             </div>    
             <div class="landing-page-gradient"></div>
-        </div>
+        </div>        
     </div>
+    <div class="content-wrapper">            
+            <?php
+            // *** PUT YOUR API KEY HERE:
+            $myAPIKey = '9c9ea06b-27ba-458a-b0d8-32e5202faff5';
+            $url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest';
+            $parameters = array ( 'symbol' => 'BTC,ETH,DOGE,SHIB' );
+            
+            $headers = array ( 
+            'Accepts: application/json', 
+            'X-CMC_PRO_API_KEY: ' . $myAPIKey 
+            );
+            // query string encode the parameters
+            $qs = http_build_query($parameters);
+            // create the request URL
+            $request = "{$url}?{$qs}";
+            // Get cURL resource
+            $curl = curl_init();
+            // Set cURL options
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => $request,      // set the request URL
+            CURLOPT_HTTPHEADER => $headers, // set the headers 
+            CURLOPT_RETURNTRANSFER => 1     // ask for raw response instead of bool
+            ));
+            // Send the request, save the response
+            $response = curl_exec($curl);
+            // print json decoded response (here as an array)
+            echo '<pre>';
+            print_r(json_decode($response, true)); 
+            echo '</pre>';
+            // Close request
+            curl_close($curl);
+            ?>
+        </div>
     <div style="height: 2000px;"></div> <!-- platzhalter zum scrollen -->
   </body>
 </html>
